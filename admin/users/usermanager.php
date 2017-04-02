39f5c9970b367e8fe530c033f7e89132eb46ae4f
Manzar98 <manzarnouman@gmail.com>
2017-04-02 23:23:50 +0500

    deactivate user

--- admin/users/usermanager.php
+++ admin/users/usermanager.php
@@ -37,18 +37,32 @@ if(isset($_SESSION["message"]))
 						<th>First Name</th>
 						<th>Last Name</th>
 						<th>Email</th>
-						<th>Action</th>
-
+						<th class="text-center">Action</th>
 					</tr>
 				</thead>
 				<?php
 				$ftnresult=select('users',"","");
 
-				while ($userResult=mysqli_fetch_assoc($ftnresult)) { ?>
+				while ($userResult=mysqli_fetch_assoc($ftnresult)) { 
 
-
-				
-				<tbody>
+                           if ($userResult['deactivate']=='on') {?>
+                         
+	                <tbody>
+	                <div>
+						<tr class="tablebck">
+							<td ><?php echo $userResult['idnno']; ?></td>
+							<td class="capitalize"><?php echo $userResult['fname'] ?></td>
+							<td class="capitalize"><?php echo $userResult['lname'] ?></td>
+							<td><?php echo $userResult['email']; ?></td>
+							<td class="text-center"><a  href="index.php?act=mguv&id=<?php echo $userResult['id'] ; ?>"  data-toggle="tooltip" data-placement="bottom" title="Veiw User Detail">
+								<i class="fa fa-eye" aria-hidden="true"></i>
+							</a></td>	
+						</tr>
+						</div>
+					</tbody>
+                           <?php  }else {?>
+                           	
+                           	<tbody>
 					<tr>
 						<td><?php echo $userResult['idnno']; ?></td>
 						<td class="capitalize"><?php echo $userResult['fname'] ?></td>
@@ -56,15 +70,15 @@ if(isset($_SESSION["message"]))
 						<td><?php echo $userResult['email']; ?></td>
 						<td class="text-center"><a  href="index.php?act=mguv&id=<?php echo $userResult['id'] ; ?>"  data-toggle="tooltip" data-placement="bottom" title="Veiw User Detail">
 							<i class="fa fa-eye" aria-hidden="true"></i>
-						</a>
+						</a></td>
+						
 
 						
 					</tr>
 				</tbody>
-				<?php } 
-
-
-				?>
+                     <?php } 
+				
+				 }?>
 
 			</table>
 		</div>
