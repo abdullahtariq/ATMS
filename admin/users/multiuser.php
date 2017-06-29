5974a973bde494f0799bf22b3e45fb23e6231554
abdullahtariq <faabdullahtariq@gmail.com>
2017-06-29 21:31:25 +0500

    Date Search Range Implemented for MultiUser

--- admin/users/multiuser.php
+++ admin/users/multiuser.php
@@ -15,12 +15,17 @@
  $currentUserIds = explode(",", $id);
  $empty_array=array();
  for ($i=0; $i < count($currentUserIds) ; $i++) {
- 	$totalSingleArray = getProjects($currentUserIds[$i]);
- 	array_push($empty_array,$totalSingleArray);
+   if(isset($_GET['date'])){
+     $totalSingleArray = getProjectsDate($currentUserIds[$i]);
+     array_push($empty_array,$totalSingleArray);
+   }else{
+     $totalSingleArray = getProjects($currentUserIds[$i]);
+     array_push($empty_array,$totalSingleArray);
+   }
  }
    // print_r($empty_array);?>
 
- <div class="container">
+ <div class="">
 
  	<div class="table-responsive tale-strip-color" id="tabular">
  		<table class="table-striped table table-wrap"   >
@@ -39,7 +44,7 @@
  					<th  colspan="6" class="text-center">HOURS</th>
  					<th  colspan="7" class="text-center">COST</th>
  				</tr>
- 			</thead> 
+ 			</thead>
 
  			<tr class="text-center">
  				<td class="bg-grey"></td>
@@ -64,19 +69,19 @@
  				<td><b>L</b></td>
  				<td><b>O</b></td>
  				<td><b>TC</b></td>
- 			</tr> 
- 			
+ 			</tr>
+
 <?php
 
 
-		for ($i=0; $i < count($empty_array); $i++) { 
+		for ($i=0; $i < count($empty_array); $i++) {
       $check=$empty_array[$i]['ticketing'];
       $check=$empty_array[$i]['training'];
       $check=$empty_array[$i]['leave'];
       $check=$empty_array[$i]['meeting'];
-      
+
    if (count($check)>0) { ?>
-             <tr class="text-center" id="selct"> 
+             <tr class="text-center" id="selct">
           <td><?php echo $empty_array[$i]['lastname'];  ?></td>
           <td><?php echo $empty_array[$i]['firstname'];  ?></td>
           <td><?php echo $empty_array[$i]['mobile'];  ?></td>
@@ -91,9 +96,9 @@
           <td><?php echo  $empty_array[$i]['training']; ?></td>
           <td><?php echo  $empty_array[$i]['meeting']; ?></td>
           <td><?php echo  $empty_array[$i]['leave']; ?></td>
-          <td><?php echo  $empty_array[$i]['others']; ?></td> 
-          <td><?php echo  $empty_array[$i]['Thours']; ?></td>  
-          <td><?php echo  $empty_array[$i]['work_c']; ?></td> 
+          <td><?php echo  $empty_array[$i]['others']; ?></td>
+          <td><?php echo  $empty_array[$i]['Thours']; ?></td>
+          <td><?php echo  $empty_array[$i]['work_c']; ?></td>
           <td><?php echo  $empty_array[$i]['Train_c']; ?></td>
           <td><?php echo  $empty_array[$i]['meet_c']; ?></td>
           <td><?php echo  $empty_array[$i]['leave_c']; ?></td>
@@ -104,16 +109,120 @@
           }else{
              echo   $empty_array[$i]['Tcost'];
           }
-           ?>  
+           ?>
           </td>
-        </tr> 
+        </tr>
 
-<?php } 
+<?php }
 }
- 			
+
+      function getProjectsDate($id){
+        include '../../common-sql.php';
+        if($_GET['date']){
+          $sDate = $_GET['date'];
+          $explDate = explode(",",$sDate);
+          $format_T =strtotime($explDate[0]);
+          $T=date('20y-m-d',$format_T);
+
+          //$from_Date=$_POST['post_at_to_date'];
+          $format_F=strtotime($explDate[1]);
+          $F=date('20y-m-d',$format_F);
+          $query='SELECT * FROM projects WHERE strtdate BETWEEN "'.$T.'" AND "'.$F.'" AND user_id="'.$id.'" ORDER BY strtdate desc';
+          $agentResult=mysqli_query($conn,$query);
+
+          $userQuery="SELECT * FROM users WHERE user_id IN ($id) ";
+          $userSlct=mysqli_query($conn,$userQuery);
+          $u=mysqli_fetch_assoc($userSlct);
+
+          $continents=array("AS"=>"Asia","EU"=>"Europe","AM"=>"America","AF"=>"Africa","HQ"=>"HQ");
+          $roleINFO= array("CH"=>"Chief","HM"=>"Hub Manager","AG"=>"Agent","HS"=>"Hub Supervisor","SF"=>"Staff");
+
+          $Twork=0;
+          $Ttrain=0;
+          $Tmeet=0;
+          $Tleave=0;
+          $Tother=0;
+          $T_cost=0;
+          $Tarray = array();
+
+          while  ($ag=mysqli_fetch_assoc($agentResult))  {
+
+            $f_name=$u['firstname'];
+            $l_name=$u['lastname'];
+            $mbl=$u['mobile'];
+            $hub=$continents[$u['continents']];
+            $role=$roleINFO[$u['inforole']];
+            $fin_data=$u['finanasdata'];
+            $act="N/A";
+            $fin_opt=$u['finanasoption'];
+            $con_srt=$u['contractstart'];
+            $con_end=$u['contractend'];
+            $Twork=$Twork+ $ag['ticketing'];
+            $Ttrain=$Ttrain+ $ag['training'];
+            $Tmeet=$Tmeet+ $ag['meeting'];
+            $Tleave=$Tleave+ $ag['leave'];
+            $Tother=$Tother+ $ag['others'];
+            $Thours=$Twork+$Ttrain+$Tmeet+$Tleave+$Tother;
+            $perHC=$u['peryear'];
+            if ($u['finanasdata']=='consultants') {
+
+                             $Wcost=$Twork*$perHC;
+                             $Trcost=$Ttrain*$perHC;
+                             $Mcost=$Tmeet*$perHC;
+                             $Ocost=$Tother*$perHC;
+                             $Lcost=$perHC*$Tleave;
+
+              $T_cost=$u['peryear']*$Thours;
+            }else{
+                            $Wcost=$perHC/1600*$Twork;
+                            $Trcost=$perHC/1600*$Ttrain;
+                            $Mcost=$perHC/1600*$Tmeet;
+                            $Ocost=$perHC/1600*$Tother;
+                            $Lcost=$perHC/1600*$Tleave;
+
+              $T_cost=$u['peryear']/1600*$Thours;
+            }
+
+          }//while-end
+
+          # code...
+        if (mysqli_num_rows($agentResult)> 0) {
+
+                $Tarray =array(
+                  'ticketing' => $Twork,
+                  'training'=> $Ttrain,
+                  'meeting'=> $Tmeet,
+                  'leave'=> $Tleave,
+                  'others'=> $Tother,
+                  'Thours'=>$Thours,
+                  'work_c' =>$Wcost,
+                  'Train_c' =>$Trcost,
+                  'meet_c' =>$Mcost,
+                  'leave_c' =>$Lcost,
+                  'other_c' =>$Ocost,
+                  'Tcost' =>$T_cost,
+                  'lastname'=>$l_name,
+                  'firstname'=>$f_name,
+                  'mobile'=>$mbl,
+                  'continents'=>$hub,
+                  'inforole'=>$role,
+                  'finanasdata'=>$fin_data,
+                  'acting role'=>$act,
+                  'finanasoption'=>$fin_opt,
+                  'contractstart'=>$con_srt,
+                  'contractend'=>$con_end,
+
+                  );
+                return $Tarray;
+                # code...
+
+
+                  }
+        }
+      }
  			function getProjects($id){
  				include '../../common-sql.php';
- 				
+
  				$query="SELECT * FROM projects WHERE user_id IN ($id) ";
  				$agentResult=mysqli_query($conn,$query);
 
@@ -133,7 +242,7 @@
  				$Tarray = array();
 
  				while  ($ag=mysqli_fetch_assoc($agentResult))  {
-     
+
  					$f_name=$u['firstname'];
  					$l_name=$u['lastname'];
  					$mbl=$u['mobile'];
@@ -152,13 +261,13 @@
  					$Thours=$Twork+$Ttrain+$Tmeet+$Tleave+$Tother;
           $perHC=$u['peryear'];
  					if ($u['finanasdata']=='consultants') {
- 						
+
                            $Wcost=$Twork*$perHC;
                            $Trcost=$Ttrain*$perHC;
                            $Mcost=$Tmeet*$perHC;
                            $Ocost=$Tother*$perHC;
    						             $Lcost=$perHC*$Tleave;
-                        
+
  						$T_cost=$u['peryear']*$Thours;
  					}else{
                           $Wcost=$perHC/1600*$Twork;
@@ -204,9 +313,9 @@ if (mysqli_num_rows($agentResult)> 0) {
  				return $Tarray;
         # code...
 
- 		
- 				  } 
- 				  
+
+ 				  }
+
  			} ?>
  		</table>
  	</div>
